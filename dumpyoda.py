308bdc4c1b4907d7c65d7a9a6ccb07a7a91e6f32
Chris Pollard <cpollard@cern.ch>
2022-12-09 09:20:12 +0000

    observable values

---
+++ dumpyoda.py
@@ -0,0 +1,35 @@
+import yoda
+
+yodadict = yoda.readYODA("ATLAS_2017_I1614149.yoda")
+
+toppt = yodadict["/REF/ATLAS_2017_I1614149/d26-x01-y01"]
+ttpt = yodadict["/REF/ATLAS_2017_I1614149/d20-x01-y01"]
+ttm = yodadict["/REF/ATLAS_2017_I1614149/d24-x01-y01"]
+
+def printS(s):
+  binedges = []
+  diffxsecs = []
+  xsecs = []
+  for p in s.points():
+    diffxsecs.append(p.y())
+    binedges.append(p.xMin())
+    xsecs.append(p.y()*(p.xMax() - p.xMin()))
+
+  binedges.append(s.points()[-1].xMax())
+
+  print(binedges)
+  print(diffxsecs)
+  print(xsecs)
+  return
+
+print("toppt")
+printS(toppt)
+print("")
+
+print("ttpt")
+printS(ttpt)
+print("")
+
+print("ttm")
+printS(ttm)
+print("")
